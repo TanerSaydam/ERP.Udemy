ce94bbe019f7b2eee8145b71286674ce89cd2673
TanerSaydam <tanersaydam@gmail.com>
2024-04-21 06:29:35 +0300

    Generic Blank ve Section Component

--- ERPClient/src/app/components/home/home.component.ts
+++ ERPClient/src/app/components/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { BlankComponent } from '../blank/blank.component';
+import { SectionComponent } from '../section/section.component';
+import { SharedModule } from '../../modules/shared.module';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
