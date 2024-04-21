3214e2b076bf20f298219f0a447f4571b60fc8d3
TanerSaydam <tanersaydam@gmail.com>
2024-04-21 06:04:58 +0300

    Layout tasarımı ve routing

--- ERPClient/src/app/app.routes.ts
+++ ERPClient/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { LayoutsComponent } from './components/layouts/layouts.component';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "login",
+        component: LoginComponent
+    },
+    {
+        path: "",
+        component: LayoutsComponent,
+        children: [
+            {
+                path: "",
+                component: HomeComponent
+            }
+        ]
+    }
+];
