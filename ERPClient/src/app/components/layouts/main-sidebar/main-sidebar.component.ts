5484cab10bc7c3bbc41f1feea4b46a730de56acb
TanerSaydam <tanersaydam@gmail.com>
2024-04-21 06:16:21 +0300

    Admin LTE temasını giydirme

---
+++ ERPClient/src/app/components/layouts/main-sidebar/main-sidebar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-sidebar',
+  standalone: true,
+  imports: [],
+  templateUrl: './main-sidebar.component.html',
+  styleUrl: './main-sidebar.component.css'
+})
+export class MainSidebarComponent {
+
+}
