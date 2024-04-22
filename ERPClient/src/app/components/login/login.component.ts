adc06169839aed710badb3d56db6348a56245724
TanerSaydam <tanersaydam@gmail.com>
2024-04-22 07:14:59 +0300

    Login Page Tasarımı

--- ERPClient/src/app/components/login/login.component.ts
+++ ERPClient/src/app/components/login/login.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { SharedModule } from '../../modules/shared.module';
+import { LoginModel } from '../../models/login.model';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
+  model: LoginModel = new LoginModel();
+ 
+  signIn(){
+    
+  }
 }
